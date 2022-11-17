9238e6ba2cee219404c0ec9ccd51964a5aaf9b98
Nesibe Somuncu <69713951+Nesibes20@users.noreply.github.com>
2022-11-17 09:46:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+print("Merhaba Etiya")
+
+# string = metin ifadesi
+text = "10"
+print(text)
+
+#integer = tam sayı
+number = 20
+print(number)
+
+#double, float = kesirli sayı
+dnumber = 5.3
+print(dnumber)
+
+#matematiksel operatörler
+print(number + 3) #toplama
+print(number - 3) #çıkarma
+print(number * 3) #çarpma
+print(number / 3) #bölme
+print(number & 3) #sayının bölme işleminden kalanı verir
+
+#boolean = İki değer alır.True veya False
+isVerified = True
+
+#mantıksal, karşılaştırma operatörleri(sonuç olarak True veya False değerini verir)
+print(5 == 9) #false
+print(3 != 3) #false
+
+print(4 > 4) #false
+print(6 < 6) #false
+
+print(2 <= 2) #true
+print(2 >= 2) #true
+
+print(20%2==0) #20 sayısı 2 ye tam olarak bölündüğünden kalan olarak 0 verdiği için sonuç olarak True değerini verir.
+
+#string fomksiyonları
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("M"))
+print(text.endswith("ya"))
+
+#f' string yazımı
+name = "Nesibe"
+age = 22
+company = "Etiya"
+print(f"{name} {age} yasinda ve {company}'de calisiyor")
+
+
+
+
